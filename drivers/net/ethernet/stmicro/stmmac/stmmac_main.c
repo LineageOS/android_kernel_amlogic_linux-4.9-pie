c09a86cb1856c774628a34faec1839f178504208
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-21 04:55:15 +0000

    Revert "[ethernet] no cable is plugged in, the Ethernet does not function after standby wakeup"
    This reverts commit d90647961839144547e9b53721aff52d6397ab6c.
    
    Change-Id: I744382b88bd7baf01c6ea80cafe18e6d8d7e6198
    Reason: Needs additional work.

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1916,8 +1916,6 @@ static int stmmac_open(struct net_device *dev)
 #ifdef TX_MONITOR
 	queue_delayed_work(moniter_tx_wq, &moniter_tx_worker, HZ);
 #endif
-	stmmac_release(ndev);
-	stmmac_open(ndev);
 	return 0;
 
 lpiirq_error:
